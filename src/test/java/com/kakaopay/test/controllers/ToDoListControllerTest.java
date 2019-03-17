aa091e07d7c3efc9232b1a0f357558f2c97bdf63
Anna Kim <annajinee@gmail.com>
2019-03-17 23:04:38 +0900

    [수정사항] - 테스트 코드 작성중

--- src/test/java/com/kakaopay/test/controllers/ToDoListControllerTest.java
+++ src/test/java/com/kakaopay/test/controllers/ToDoListControllerTest.java
@@ -1,76 +1,124 @@
 package com.kakaopay.test.controllers;
 
-import com.kakaopay.test.AbstractTest;
+import com.kakaopay.test.MockMvcTest;
 import com.kakaopay.test.model.dto.ToDoAddPayload;
 import com.kakaopay.test.model.dto.ToDoUpdatePayload;
 import com.kakaopay.test.model.dto.TodoListPageResult;
-import com.kakaopay.test.model.entity.ToDoListData;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ToDoListControllerTest extends AbstractTest {
+
+@Slf4j
+public class ToDoListControllerTest extends MockMvcTest {
+
     @Override
     @Before
     public void setUp() {
         super.setUp();
     }
 
-    private String toDoUri="/todo";
+    private String toDoUri = "/todo";
+
 
     @Test
     public void getTodoList() throws Exception {
-        String uri = toDoUri + "/list/0/5";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-        System.out.println(mvcResult);
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(toDoUri + "/list/0/5")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
         String content = mvcResult.getResponse().getContentAsString();
-        TodoListPageResult[] todoListPageResults = super.mapFromJson(content, TodoListPageResult[].class);
-        assertTrue(todoListPageResults.length > 0);
+
+        TodoListPageResult todoListPageResults = super.mapFromJson(content, TodoListPageResult.class);
+
+        System.out.println("response : " + content);
+        assertTrue(todoListPageResults.getDataList() != null);
+        assertTrue(todoListPageResults.getPageInfo().getPageNumber() == 0);
+
     }
+
+
     @Test
     public void addTodo() throws Exception {
-        String uri = toDoUri;
-        ToDoAddPayload toDoUpdatePayload = new ToDoAddPayload();
-        toDoUpdatePayload.setToDo("방청소");
+
+        ToDoAddPayload toDoAddPayload = new ToDoAddPayload();
+        toDoAddPayload.setToDo("방청소");
+
         JSONArray refArray = new JSONArray();
         refArray.add(1);
         refArray.add(3);
         refArray.add(4);
-        toDoUpdatePayload.setRefId(refArray);
+        toDoAddPayload.setRefId(refArray);
 
-        String inputJson = super.mapToJson(toDoUpdatePayload);
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+        String inputJson = super.mapToJson(toDoAddPayload);
+
+        System.out.println("request : " + inputJson);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(toDoUri)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(inputJson)).andReturn();
 
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
-        String content = mvcResult.getResponse().getContentAsString();
-        assertEquals(content, "정상 등록");
+        System.out.println("response : " + mvcResult.getResponse().getContentAsString());
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals(mvcResult.getResponse().getContentAsString(), "정상 등록");
     }
+
+
     @Test
     public void updateTodo() throws Exception {
-    String uri = toDoUri +"/1";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-        System.out.println(mvcResult);
+        String uri = toDoUri + "/0";
+
+        ToDoUpdatePayload toDoUpdatePayload = new ToDoUpdatePayload();
+        toDoUpdatePayload.setToDo("유자차담그기");
+
+        String inputJson = super.mapToJson(toDoUpdatePayload);
+
+        System.out.println("request : " + inputJson);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        System.out.println("response : " + mvcResult.getResponse().getContentAsString());
+
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
         String content = mvcResult.getResponse().getContentAsString();
-        TodoListPageResult[] todoListPageResults = super.mapFromJson(content, TodoListPageResult[].class);
-        assertTrue(todoListPageResults.length > 0);
-
+        assertEquals(content, "true");
     }
 
+
+    @Test
+    public void updateEndYn() throws Exception {
+        String uri = toDoUri + "/0";
+
+        ToDoUpdatePayload toDoUpdatePayload = new ToDoUpdatePayload();
+        toDoUpdatePayload.setEndYn("Y");
+
+        String inputJson = super.mapToJson(toDoUpdatePayload);
+
+        System.out.println("request : " + inputJson);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        System.out.println("response : " + mvcResult.getResponse().getContentAsString());
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "true");
+    }
 
 }
